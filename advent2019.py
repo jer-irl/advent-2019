62523b2182e7828d0e92dbd02d716b40d13abddc
Jeremy Schroeder <jpschroeder2014@gmail.com>
2019-12-01 13:39:20 -0500

    Update harness for two puzzles each day

--- advent2019.py
+++ advent2019.py
@@ -1,5 +1,6 @@
 import argparse
 import importlib
+import itertools
 import os
 import pathlib
 import sys
@@ -7,28 +8,28 @@ import sys
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("days", nargs="*")
+    parser.add_argument("puzzles", nargs="*")
     parser.add_argument("--data-file")
     args = parser.parse_args()
 
     script_dir = pathlib.Path(__file__).parent
     solution_filenames = (f for f in os.listdir(script_dir) if "sol" in f)
     solved_days = sorted(f[3:-3] for f in solution_filenames)
-    days = args.days or solved_days
+    puzzles = args.puzzles or solved_days
 
-    for day_arg in days:
-        day = f"{int(day_arg):02}"
+    for puzzle_arg in puzzles:
+        puzzle = f"{int(puzzle_arg[:-1]):02}{puzzle_arg[-1]}"
         if args.data_file is not None:
             data_filename = args.data_file
         else:
-            data_filename = f"data/{day}.txt"
+            data_filename = f"data/{puzzle}.txt"
         with open(data_filename, "r") as data_file:
             data = data_file.read()
 
-        solution_module = importlib.import_module(f"sol{day}")
+        solution_module = importlib.import_module(f"sol{puzzle}")
         result = solution_module.run(data)
 
-        print(f"Day {day} answer with data file {data_filename}:")
+        print(f"Puzzle {puzzle} answer with data file {data_filename}:")
         print(result)
 
 
